8913e0a7156b3635496711f223e65aa7ea4f9076
mac <ben.mcdonald@hixme.com>
2019-02-02 22:33:55 -0800

    ready

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+// app.set('views', path.join(process.env.CONTEXT_DIR, '/views'));
+
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
